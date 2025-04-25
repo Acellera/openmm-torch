7c9431c8e98a7f8067d60068942592ac72c2b646
Stefan Doerr <stefdoerr@gmail.com>
2025-04-25 20:32:02 +0300

    one more try

--- python/cibuildwheel_support/before_build_windows.sh
+++ python/cibuildwheel_support/before_build_windows.sh
@@ -24,8 +24,8 @@ if [ "$ACCELERATOR" == "cu118" ] || [ "$ACCELERATOR" == "cu126" ] || [ "$ACCELER
     ARCH_LIST_FMT=$(python -c "import torch; print(';'.join([y for y in [x[3:] for x in torch._C._cuda_getArchFlags().split() if x.startswith('sm_')]]))")
     CMAKE_FLAGS="    -DTORCH_CUDA_ARCH_LIST=${ARCH_LIST}"
     CMAKE_FLAGS+="    -DCMAKE_CUDA_ARCHITECTURES=${ARCH_LIST_FMT}"
-    CMAKE_FLAGS+="    -DCUDA_TOOLKIT_ROOT_DIR=\"${CUDA_HOME}\""
-    CMAKE_FLAGS+="    -DCMAKE_CUDA_COMPILER=\"${CUDA_HOME}\\bin\\nvcc\""
+    CMAKE_FLAGS+="    -DCUDA_TOOLKIT_ROOT_DIR=\"${CUDA_HOME//\\/\\\\}\""
+    CMAKE_FLAGS+="    -DCMAKE_CUDA_COMPILER=\"${CUDA_HOME//\\/\\\\}\\bin\\nvcc\""
 fi
 
 

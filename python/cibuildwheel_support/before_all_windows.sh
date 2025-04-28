752bd78f16d4186254a410a1834214184c283bf3
Stefan Doerr <stefdoerr@gmail.com>
2025-04-28 15:59:20 +0300

    installing nvtx which was missing for CUDA::nvToolsExt

--- python/cibuildwheel_support/before_all_windows.sh
+++ python/cibuildwheel_support/before_all_windows.sh
@@ -14,7 +14,7 @@ extra-index-url = https://download.pytorch.org/whl/cpu
 
 if [ "$ACCELERATOR" == "cu118" ]; then
     curl --netrc-optional -L -nv -o cuda.exe https://developer.download.nvidia.com/compute/cuda/11.8.0/local_installers/cuda_11.8.0_522.06_windows.exe
-    ./cuda.exe -s nvcc_11.8 nvrtc_11.8 nvrtc_dev_11.8 cudart_11.8 cufft_11.8 cufft_dev_11.8 cuda_profiler_api_11.8
+    ./cuda.exe -s nvcc_11.8 nvrtc_11.8 nvrtc_dev_11.8 cudart_11.8 cufft_11.8 cufft_dev_11.8 cuda_profiler_api_11.8 nvtx_11.8
     rm cuda.exe
     # Move CUDA folder to a path without spaces
     mv "/c/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.8" /d/cuda
@@ -25,7 +25,7 @@ extra-index-url = https://download.pytorch.org/whl/cu118
                   https://us-central1-python.pkg.dev/pypi-packages-455608/cu118/simple" > "C:\ProgramData\pip\pip.ini"
 elif [ "$ACCELERATOR" == "cu126" ]; then
     curl --netrc-optional -L -nv -o cuda.exe https://developer.download.nvidia.com/compute/cuda/12.6.0/local_installers/cuda_12.6.0_560.76_windows.exe
-    ./cuda.exe -s nvcc_12.6 nvrtc_12.6 nvrtc_dev_12.6 cudart_12.6 cufft_12.6 cufft_dev_12.6 cuda_profiler_api_12.6
+    ./cuda.exe -s nvcc_12.6 nvrtc_12.6 nvrtc_dev_12.6 cudart_12.6 cufft_12.6 cufft_dev_12.6 cuda_profiler_api_12.6 nvtx_12.6
     rm cuda.exe
     # Move CUDA folder to a path without spaces
     mv "/c/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v12.6" /d/cuda
@@ -36,7 +36,7 @@ extra-index-url = https://download.pytorch.org/whl/cu126
                   https://us-central1-python.pkg.dev/pypi-packages-455608/cu126/simple" > "C:\ProgramData\pip\pip.ini"
 elif [ "$ACCELERATOR" == "cu128" ]; then
     curl --netrc-optional -L -nv -o cuda.exe https://developer.download.nvidia.com/compute/cuda/12.8.1/local_installers/cuda_12.8.1_572.61_windows.exe
-    ./cuda.exe -s nvcc_12.8 nvrtc_12.8 nvrtc_dev_12.8 cudart_12.8 cufft_12.8 cufft_dev_12.8 cuda_profiler_api_12.8
+    ./cuda.exe -s nvcc_12.8 nvrtc_12.8 nvrtc_dev_12.8 cudart_12.8 cufft_12.8 cufft_dev_12.8 cuda_profiler_api_12.8 nvtx_12.8
     rm cuda.exe
     # Move CUDA folder to a path without spaces
     mv "/c/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v12.8" /d/cuda

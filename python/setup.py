dcf4794d3354b09b184005ad2468b5d8ab337dac
Stefan Doerr <stefdoerr@gmail.com>
2025-04-29 11:21:39 +0300

    try with all the libraries

--- python/setup.py
+++ python/setup.py
@@ -12,17 +12,16 @@ torch_dir, _ = os.path.split('@TORCH_LIBRARY@')
 
 extra_compile_args = ['-std=c++17', '-D_GLIBCXX_USE_CXX11_ABI=1']
 extra_link_args = []
-libraries = ['OpenMM', 'OpenMMTorch', 'c10', 'torch']
+libraries = ['OpenMM', 'OpenMMTorch', 'c10', 'torch', 'torch_cpu']
 if os.environ.get("ACCELERATOR", "").startswith("cu"):
-    libraries += ['torch_cuda', 'c10_cuda']
-else:
-    libraries += ['torch_cpu']
+    libraries += ['cudart', 'c10_cuda', 'torch_cuda']
+
 runtime_library_dirs = ["$ORIGIN/../openmm/lib", "$ORIGIN/../torch/lib"]
 
 # For Windows change the compiler flag to /std:c++17
 define_macros = []
 if platform.system() == 'Windows':
-    extra_compile_args += ['/std:c++17', '/EHsc']
+    extra_compile_args = ['/std:c++17', '/EHsc']
     runtime_library_dirs = None
     define_macros.append( ('WIN32', None) )
     define_macros.append( ('_WINDOWS', None) )

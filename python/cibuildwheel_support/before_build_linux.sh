752c76839084cd956ec8d7aea644ff1c4fb57c66
Stefan Doerr <stefdoerr@gmail.com>
2025-04-29 12:06:34 +0300

    update CI

--- python/cibuildwheel_support/before_build_linux.sh
+++ python/cibuildwheel_support/before_build_linux.sh
@@ -12,6 +12,7 @@ if [ "$ACCELERATOR" == "cu118" ] || [ "$ACCELERATOR" == "cu126" ] || [ "$ACCELER
     ARCH_LIST=$(python -c "import torch; print(';'.join([f'{y[:-1]}.{y[-1]}' for y in [x[3:] for x in torch._C._cuda_getArchFlags().split() if x.startswith('sm_')]]))")
     # CMakeLists.txt seems to ignore the CMAKE_CUDA_ARCHITECTURES variable, instead, it is overwritten by TORCH_CUDA_ARCH_LIST
     ARCH_LIST_FMT=$(python -c "import torch; print(';'.join([y for y in [x[3:] for x in torch._C._cuda_getArchFlags().split() if x.startswith('sm_')]]))")
+    export CUDA_HOME="/usr/local/cuda"
     CMAKE_FLAGS="    -DTORCH_CUDA_ARCH_LIST=${ARCH_LIST}"
     CMAKE_FLAGS+="    -DCMAKE_CUDA_ARCHITECTURES=${ARCH_LIST_FMT}"
     CMAKE_FLAGS+="    -DCUDA_TOOLKIT_ROOT_DIR=${CUDA_HOME}"

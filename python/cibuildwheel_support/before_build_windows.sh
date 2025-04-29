a9c83aeed7d43b802610eda3177013c5222a2906
Stefan Doerr <stefdoerr@gmail.com>
2025-04-29 18:43:24 +0300

    Revert "maybe windows cp doesn't overwrite files"
    This reverts commit 0dd139aef94a2d8609c5fb3bb7dde3983eaa62de.

--- python/cibuildwheel_support/before_build_windows.sh
+++ python/cibuildwheel_support/before_build_windows.sh
@@ -4,7 +4,6 @@ set -e
 set -x
 
 # Cleanup the python folder from previous build
-rm -f python/setup.py
 cp /d/setup.py.bkp python/setup.py
 rm -f python/TorchPluginWrapper.cpp
 rm -f python/openmmtorch.py

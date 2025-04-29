5a3048a621524b10d20b8f9028b66e39aa19ae8d
Stefan Doerr <stefdoerr@gmail.com>
2025-04-29 10:17:56 +0300

    try a few more things

--- python/cibuildwheel_support/before_build_windows.sh
+++ python/cibuildwheel_support/before_build_windows.sh
@@ -8,7 +8,7 @@ pip install torch openmm==8.2.1rc1
 PYTHONPREFIX=$(python -c 'import site; print(site.getsitepackages()[0])')
 SITE_PACKAGES="$PYTHONPREFIX/Lib/site-packages/"
 
-
+CMAKE_FLAGS="-DENABLE_CUDA=OFF"
 if [ "$ACCELERATOR" == "cu118" ] || [ "$ACCELERATOR" == "cu126" ] || [ "$ACCELERATOR" == "cu128" ]; then
     ARCH_LIST=$(python -c "import torch; print(';'.join([f'{y[:-1]}.{y[-1]}' for y in [x[3:] for x in torch._C._cuda_getArchFlags().split() if x.startswith('sm_')]]))")
     # CMakeLists.txt seems to ignore the CMAKE_CUDA_ARCHITECTURES variable, instead, it is overwritten by TORCH_CUDA_ARCH_LIST
@@ -19,11 +19,13 @@ if [ "$ACCELERATOR" == "cu118" ] || [ "$ACCELERATOR" == "cu126" ] || [ "$ACCELER
     CMAKE_FLAGS+=" -DCUDA_TOOLKIT_ROOT_DIR=/d/cuda"
     CMAKE_FLAGS+=" -DCUDA_NVCC_EXECUTABLE=/d/cuda/bin/nvcc.exe"
     CMAKE_FLAGS+=" -DCMAKE_CUDA_COMPILER=/d/cuda/bin/nvcc.exe"
+    CMAKE_FLAGS+=" -DCUDA_DRIVER_LIBRARY_PATH=/d/cuda/lib/x64/"
+    export TORCH_NVCC_FLAGS="-Xfatbin -compress-all"
 
     export CUDA_PATH="/d/cuda"
 fi
 
-
+CMAKE_GENERATOR=Ninja
 OPENCL_PATH="$(pwd)/OpenCL-SDK-v2024.10.24-Win-x64"
 
 # Configure build with Cmake
@@ -38,13 +40,13 @@ cmake .. -G "NMake Makefiles JOM" \
     -DCMAKE_INSTALL_PREFIX=../install \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_PREFIX_PATH=${SITE_PACKAGES} \
-    -DCUDA_DRIVER_LIBRARY_PATH=/d/cuda/lib/x64/ \
     -DUSE_SYSTEM_NVTX=1 \
     -DCMAKE_CXX_COMPILER=cl.exe \
     -DCMAKE_C_COMPILER=cl.exe \
     -DOPENMM_DIR=${SITE_PACKAGES}/openmm \
     -DPYTORCH_DIR=${SITE_PACKAGES}/torch \
     -DTorch_DIR=${SITE_PACKAGES}/torch/share/cmake/Torch \
+    -DNN_BUILD_PYTHON_WRAPPERS=ON \
     -DNN_BUILD_OPENCL_LIB=ON \
     -DOPENCL_INCLUDE_DIR="${OPENCL_PATH}/include" \
     -DOPENCL_LIBRARY="${OPENCL_PATH}/lib/OpenCL.lib" \
